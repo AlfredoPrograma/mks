9179115eced34b71743c360084407c26c1269405
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2026-07-16 17:10:51 -0400

    cicd: update workflow to consider updated aws vars
    merge registry and repo tags into repository url

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,7 +3,12 @@ output "container_registry_name" {
   description = "Name assigned to the Container Registry"
 }
 
-output "container_registry_url" {
+output "container_registry_repository_url" {
   value = aws_ecr_repository.this.repository_url
-  description = "URL assigned to the Container Registry"
+  description = "URL assigned to the Container Registry Repository"
+}
+
+output "workflows_role_arn" {
+  value = aws_iam_role.workflows.arn
+  description = "ARN of the role assumed over workflows"
 }
